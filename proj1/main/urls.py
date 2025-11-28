5c298f27e6f40113268a5a01406a64d48e0518bc
IzuneMai <izunemai13@gmail.com>
2025-11-28 20:36:11 +0300

    created home page and nov-bar

--- proj1/main/urls.py
+++ proj1/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('profile', views.profile)
+    path('', views.index, name='home'),
+    path('profile', views.profile, name='profile')
 ]
