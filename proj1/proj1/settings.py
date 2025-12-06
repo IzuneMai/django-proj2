8921fe04095abf518d518ad5b3d5c0da506405e8
IzuneMai <izunemai13@gmail.com>
2025-12-06 17:06:36 +0300

    bagfix

--- proj1/proj1/settings.py
+++ proj1/proj1/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY') 
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3qs8u#yius_h#=fvy1j+f2se*bxtt%^eq*btc!q*8xt+v(*p93') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
