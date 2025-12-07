adf95d1463028c827161fa2ec2e456d9c54c8966
IzuneMai <izunemai13@gmail.com>
2025-12-07 18:08:08 +0300

    pr

--- proj1/proj1/settings.py
+++ proj1/proj1/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-3qs8u#yius_h#=fvy1j+f2se*bxtt%^eq*btc!q*8xt+v(*p93') 
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = True     
 
 ALLOWED_HOSTS = []
 
